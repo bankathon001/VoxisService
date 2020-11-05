20a640bb923a397c003e1fc75ad4297e93afc56f
Deepak Goyal <Deepak.Goyal@ad.fc.net>
2020-11-05 11:11:13 +0530

    add spring project

---
+++ src/main/java/com/axis/VoxisService/VoxisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.axis.VoxisService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoxisServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axis/VoxisService/VoxisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axis.VoxisService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoxisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
